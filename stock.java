f3ab8d060ff31b2448ad6cf95a32dde887c6e2ef
Jin <jinwoo.hwang99@gmail.com>
2020-08-22 23:19:27 -0400

    Added parameters to stock class

--- stock.java
+++ stock.java
@@ -1,2 +1,8 @@
 public class Stock {
+    public int num_of_shares;
+    public double avg_price;
+    public double current_price;
+    public double yearly_high;
+    public double total_val;
+    public double if_yearly_high_total;
 }
